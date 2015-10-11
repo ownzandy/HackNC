8c3adb0bf38eefd6c1047f28f35233827402ed95
Thomas <tzl2@duke.edu>
2015-10-11 02:07:11 -0400

    camera and message

--- ParseStarterProject/src/main/java/com/parse/starter/SendMsgActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/SendMsgActivity.java
@@ -1,21 +1,39 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SendMsgActivity extends ActionBarActivity {
     private EditText myText;
     private ImageButton next;
     private ImageButton pic;
+    private ImageView fromCam;
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri fileUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +41,105 @@ public class SendMsgActivity extends ActionBarActivity {
         setContentView(R.layout.activity_send_msg);
 
         next = (ImageButton) findViewById(R.id.nextButton);
-        pic = (ImageButton) findViewById(R.id.nextButton);
+        pic = (ImageButton) findViewById(R.id.picButton);
         myText = (EditText) findViewById(R.id.editText);
+        //test------------------
 
         setupUI(findViewById(R.id.parent));
+        nextOnClick();
     }
+
+    public void startCamera(View v){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+        Log.d("This is the one", fileUri.toString());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+//                Toast.makeText(this, "Image saved to:\n" +
+//                        fileUri.toString(), Toast.LENGTH_LONG).show();
+                //call image PROGRAMMATICALLY
+                embedImage();
+
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
+    }
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+        Log.d("MainActivity", mediaFile.toString());
+        return mediaFile;
+
+    }
+
+
+    public void embedImage(){
+
+            RelativeLayout myLayout = (RelativeLayout) findViewById(R.id.parent);
+
+            ImageView imageView = new ImageView(this);
+            imageView.setId(R.id.image_id);
+            //set imageView to pic from camera
+            imageView.setImageURI(fileUri);
+            //setting image resource EDIT------------------------
+//            imageView.setImageResource(R.drawable.forest);
+            RelativeLayout.LayoutParams imageViewLayoutParams
+                    = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+            imageViewLayoutParams.addRule(RelativeLayout.BELOW, R.id.editText);
+            imageViewLayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            imageView.setLayoutParams(imageViewLayoutParams);
+
+            myLayout.addView(imageView);
+            setContentView(myLayout);
+        }
+
 
     public void setupUI(View view) {
         //Set up touch listener for non-text box views to hide keyboard.
@@ -59,6 +171,24 @@ public class SendMsgActivity extends ActionBarActivity {
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
 
+    public void nextOnClick() {
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), SendActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+//    public void picOnClick() {
+//        pic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getBaseContext(), camActivity.class);
+//                startActivity(i);
+//            }
+//        });
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
